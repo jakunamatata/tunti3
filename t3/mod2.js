7164ac3de80d76ec443d28121f243dddbd454a37
Oscar Lemström <oscarlemstrom@Oscars-MacBook-Pro-2.local>
2014-09-09 19:44:53 +0300

    use formatting library

--- t3/mod2.js
+++ t3/mod2.js
@@ -1,7 +1,13 @@
 var mod1 = require('./mod1.js');
+var strftime = require("strftime");
 var path = 'mod2.js';
+
+var format = function (date) {
+	return strftime("%H:%M %d.%m.%Y", date)
+}
+
 var date = mod1.getTimeStamp(path, function(date) {
-	console.log("Last modified: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+	console.log("Last modified: " + format(new Date(date)));
 });
 
 
